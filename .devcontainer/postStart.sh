e0a1b4a0b26e236c852d7a69e548ef859ed52d5d
Ben Greenberg <hello@yalladevrel.com>
2025-03-17 18:53:26 +0200

    add postStart script

---
+++ .devcontainer/postStart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Export the PATH
+export PATH=/home/gitpod/.cargo/bin:$PATH
+
+# Start Couchbase server
+sudo /entrypoint.sh couchbase-server &
+
+# Wait for the server to start
+sleep 30
+
+# Run Couchbase shell commands
+cbsh -c "source bucketSetup.nu; dbSetup $COUCHBASE_DEFAULT_BUCKET $COUCHBASE_DEFAULT_SCOPE $COUCHBASE_DEFAULT_COLLECTION"
+
+# Start the development environment
+just dev
